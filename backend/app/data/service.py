6825853877c8053b953bb8c97683e361bdd0690c
marcqavm <138325579+marcqavm@users.noreply.github.com>
2025-05-19 14:39:46 -0300

    atualização do serviçe.py integrando ele ao tratamento de dados

--- backend/app/data/service.py
+++ backend/app/data/service.py
@@ -1,5 +1,11 @@
 from . import external_api, schemas
 from typing import Optional
+from app.data.tratamento_dados import (
+    padronizar_partidos,
+    formatar_datas,
+    ordenar_por_nome
+)
+import pandas as pd
 
 async def listar_deputados(
     id: Optional[int] = None,
@@ -7,13 +13,23 @@ async def listar_deputados(
     sigla_uf: Optional[str] = None,
     sigla_partido: Optional[str] = None
 ) -> schemas.RespostaDeputados:
+   
     dados = await external_api.buscar_deputados(
         id=id,
         nome=nome,
         sigla_uf=sigla_uf,
         sigla_partido=sigla_partido
     )
-    return schemas.RespostaDeputados(**dados)
 
-async def obter_deputado_por_id(deputado_id: int):
-    return await external_api.buscar_deputado_por_id(deputado_id)
+   
+    df = pd.DataFrame(dados['dados'])
+
+   
+    df = padronizar_partidos(df)
+    df = formatar_datas(df, 'dataNascimento')
+    df = ordenar_por_nome(df)
+
+    # Converte de volta para dicionário
+    dados['dados'] = df.to_dict(orient="records")
+
+    return schemas.RespostaDeputados(**dados)

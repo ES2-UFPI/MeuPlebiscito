97546089c8227d2fc8172dfbae1938bb71710537
KauaMach <kaua.silva@ufpi.edu.br>
2025-05-18 21:53:52 -0300

    ..

--- backend/app/data/external_api.py
+++ backend/app/data/external_api.py
@@ -30,4 +30,5 @@ async def buscar_deputado_por_id(deputado_id: int):
         resposta = await cliente.get(f"{URL_BASE}/deputados/{deputado_id}")
         resposta.raise_for_status()
         return resposta.json()
-    test
+    
+    #ssds

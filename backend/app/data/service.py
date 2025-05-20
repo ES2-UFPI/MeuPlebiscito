2886a03a208c5f3fc53919c5462924af6179f89f
marcqavm <138325579+marcqavm@users.noreply.github.com>
2025-05-20 09:06:45 -0300

    Update service.py filtros

--- backend/app/data/service.py
+++ backend/app/data/service.py
@@ -11,9 +11,9 @@ async def listar_deputados(
     id: Optional[int] = None,
     nome: Optional[str] = None,
     sigla_uf: Optional[str] = None,
-    sigla_partido: Optional[str] = None
+    sigla_partido: Optional[str] = None,
+    ordenar_por: Optional[str] = None
 ) -> schemas.RespostaDeputados:
-   
     dados = await external_api.buscar_deputados(
         id=id,
         nome=nome,
@@ -21,15 +21,24 @@ async def listar_deputados(
         sigla_partido=sigla_partido
     )
 
-   
     df = pd.DataFrame(dados['dados'])
 
-   
     df = padronizar_partidos(df)
     df = formatar_datas(df, 'dataNascimento')
-    df = ordenar_por_nome(df)
 
-    # Converte de volta para dicionário
+    if id:
+        df = df[df['id'] == id]
+    if nome:
+        df = df[df['nome'].str.contains(nome, case=False, na=False)]
+    if sigla_uf:
+        df = df[df['siglaUf'] == sigla_uf.upper()]
+    if sigla_partido:
+        df = df[df['siglaPartido'] == sigla_partido.upper()]
+
+    if ordenar_por == "nome":
+        df = df.sort_values(by='nome')
+    elif ordenar_por == "partido":
+        df = df.sort_values(by='siglaPartido')
+
     dados['dados'] = df.to_dict(orient="records")
-
     return schemas.RespostaDeputados(**dados)

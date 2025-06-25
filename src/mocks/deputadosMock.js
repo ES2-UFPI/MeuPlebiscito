564d5de77ac99420bbde53f4bbfca996a30a8162
marcqavm <138325579+marcqavm@users.noreply.github.com>
2025-06-25 15:24:32 -0300

    Create deputadosMock.js

---
+++ src/mocks/deputadosMock.js
@@ -0,0 +1,14 @@
+export const deputados = [
+  {
+    nome: "Marcos Victor",
+    siglaPartido: "PT",
+    siglaUf: "PI",
+    email: "marcos.victor@camara.leg.br"
+  },
+  {
+    nome: "Carlos Silva",
+    siglaPartido: "PL",
+    siglaUf: "SP",
+    email: "carlos.silva@camara.leg.br"
+  }
+];

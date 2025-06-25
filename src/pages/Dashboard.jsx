6effa42341ab347ee3f96c858482d48722bb892b
marcqavm <138325579+marcqavm@users.noreply.github.com>
2025-06-25 15:37:51 -0300

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,28 +1,27 @@
 import React from "react";
 import BarChartComponent from "../components/BarChart";
-import { dataDeputados } from "../mocks/DashboardMock";
 import DeputadoCard from "../components/DeputadoCard";
-import { deputados } from "../mock/deputadosMock";
+import { dataDeputados } from "../mock/mockData";
+import { deputadoMockData } from "../mock/DeputadoMock";
 
 const Dashboard = () => {
   return (
     <div style={{ padding: "20px" }}>
-      <h2>Dashboard — Gráficos Estáticos</h2>
+      <h2>Dashboard — Meu Plebiscito</h2>
+
+      {/* Gráfico de Barras */}
       <BarChartComponent
         data={dataDeputados}
         dataKeyX="partido"
         dataKeyY="quantidade"
         title="Deputados por Partido"
       />
-      <h2>Deputados</h2>
-{deputados.map((dep, index) => (
-  <DeputadoCard
-    key={index}
-    nome={dep.nome}
-    partido={dep.siglaPartido}
-    uf={dep.siglaUf}
-    email={dep.email}
-  />
+
+      {/* Lista de Deputados */}
+      <h2 style={{ marginTop: "40px" }}>Deputados</h2>
+      {Object.values(deputadoMockData).map((dep) => (
+        <DeputadoCard key={dep.id} deputado={dep} />
+      ))}
     </div>
   );
 };
